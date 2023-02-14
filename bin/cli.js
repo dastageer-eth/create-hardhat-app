698447ee9573d0cdf235eb3f2db63b11eeee8b43
Dastageercz <dastageer.sayed@codezeros.com>
2023-02-14 12:01:31 +0530

    delete git remote file

--- bin/cli.js
+++ bin/cli.js
@@ -6,7 +6,7 @@ const runCommand = (command) => {
   try {
     execSync(`${command}`, { stdio: "inherit" });
   } catch (e) {
-    console.log(`%cError to execute ${command}`, 'color: red', e);
+    console.log(`%cError to execute ${command}`, "color: red", e);
     return false;
   }
   return true;
@@ -16,15 +16,18 @@ const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/dastageer-eth/create-hardhat-app ${repoName}`;
 const installDepsCommand = `cd ${repoName} && touch .env && rm -rf .git`;
 
-console.log(`%cInitializing the template with name ${repoName}`, 'color: blue');
+console.log(`%cInitializing the template with name ${repoName}`, "color: blue");
 const checkOut = runCommand(gitCheckoutCommand);
 if (!checkOut) process.exit(-1);
 
-console.log(`%cInstalling dependencies for ${repoName}`, 'color: blue');
+console.log(`%cInstalling dependencies for ${repoName}`, "color: blue");
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(-1);
 
-console.log(`%cCongratualtions! You are ready. Follow the following to start`, 'color: green; font-size: larger');
-console.log(`%ccd ${repoName} `, 'color: blue');
-console.log(`%cnpm install`, 'color: blue');
-console.log(`%cgit checkout -b dev`, 'color: blue');
+console.log(
+  `%cCongratualtions! You are ready. Follow the following to start`,
+  "color: green; font-size: larger"
+);
+console.log(`%ccd ${repoName} `, "color: blue");
+console.log(`%cnpm install`, "color: blue");
+console.log(`%cgit checkout -b dev`, "color: blue");
